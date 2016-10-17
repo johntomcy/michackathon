73d97aa511358d9e78fbc974e9139283b7496adf
pankajmisra <misrapankaj5@gmail.com>
2016-10-17 16:17:44 +0400

    fixes to travel history

---
+++ michackathon-travel-history-api/src/main/java/com/michackathon/api/controllers/rest/TravelHistoryRestController.java
@@ -0,0 +1,17 @@
+package com.michackathon.api.controllers.rest;
+
+
+import com.michackathon.dto.commons.ValueObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+
+@RestController
+@RequestMapping("/travel-history")
+public class TravelHistoryRestController {
+
+
+
+}
