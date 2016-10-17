a330f5a7fa792a8404a55da3b1c1cb075aecf622
Tomcy <tjohn@Tomcys-MacBook-Pro.local>
2016-10-17 20:54:55 +0400

    Added Hystrix command and enabled circuit breaker in user-api when going to http://localhost:8101/hystrix.stream stream is coming but was not able to visualize in hysteria dashboard

--- michackathon-user-api/src/main/java/com/michackathon/MichackathonUserApiApplication.java
+++ michackathon-user-api/src/main/java/com/michackathon/MichackathonUserApiApplication.java
@@ -3,16 +3,18 @@ package com.michackathon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
- * Created by Tomasz Kucharzyk
+ * Created by Tomcy John
  */
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
 @EnableCaching
+@EnableCircuitBreaker
 public class MichackathonUserApiApplication {
 
     public static void main(String[] args) {
