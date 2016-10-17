5f8d991cf2eb45116c8d34a1f5a9c830b3217e83
Tomcy <tjohn@Tomcys-MacBook-Pro.local>
2016-10-17 18:16:41 +0400

    hystrix dashboard added

--- michackathon-hystrix-dashboard/src/main/java/com/michackathon/MichackathonHystrixDashboardApplication.java
+++ michackathon-hystrix-dashboard/src/main/java/com/michackathon/MichackathonHystrixDashboardApplication.java
@@ -2,25 +2,16 @@ package com.michackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Created by Tomcy John
- */
+
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 public class MichackathonHystrixDashboardApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MichackathonHystrixDashboardApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(MichackathonHystrixDashboardApplication.class, args);
+	}
 }
