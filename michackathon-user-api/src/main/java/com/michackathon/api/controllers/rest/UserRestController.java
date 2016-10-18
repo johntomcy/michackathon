6fd485837a59921ec55aaa074c402f73fb7cba02
Tomcy <tjohn@Tomcys-MacBook-Pro.local>
2016-10-18 18:32:43 +0400

    elk setup

--- michackathon-user-api/src/main/java/com/michackathon/api/controllers/rest/UserRestController.java
+++ michackathon-user-api/src/main/java/com/michackathon/api/controllers/rest/UserRestController.java
@@ -5,6 +5,8 @@ import com.michackathon.dto.commons.ValueObject;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.Principal;
 
@@ -16,9 +18,12 @@ import java.security.Principal;
 @RequestMapping("/user/")
 public class UserRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserRestController.class);
+
     @RequestMapping("/title")
     @HystrixCommand(fallbackMethod = "defaultUserTitle")
     public ValueObject getTitle(Principal user) {
+        log.info("Inside getTitle method..");
         return new ValueObject("API SERVER IS ALIVE" + (user == null ? "" : " " + user.getName()));
     }
 
