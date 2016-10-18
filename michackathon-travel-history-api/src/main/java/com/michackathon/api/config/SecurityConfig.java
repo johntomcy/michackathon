1d38de538317baccd21ca411cbca3b0a1d77181e
pankajmisra <misrapankaj5@gmail.com>
2016-10-18 18:33:27 +0400

    added security config

--- michackathon-travel-history-api/src/main/java/com/michackathon/api/config/SecurityConfig.java
+++ michackathon-travel-history-api/src/main/java/com/michackathon/api/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig implements ResourceServerConfigurer {
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId("michackathon-travel-history-api");
+        resources.resourceId("michackathon-api");
     }
 
     @Override
