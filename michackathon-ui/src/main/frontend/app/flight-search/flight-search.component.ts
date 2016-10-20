8f01ca1c9df15d1ca210e0827d7c6e5b3456c7b3
Shahji <s411350@emirates.com>
2016-10-20 15:15:56 +0400

    UI updated

--- michackathon-ui/src/main/frontend/app/flight-search/flight-search.component.ts
+++ michackathon-ui/src/main/frontend/app/flight-search/flight-search.component.ts
@@ -1,15 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
+import {AuthService} from '../shared/auth/auth.service';
+/* import {DataTableDirectives} from 'angular2-datatable/datatable'; */
 
 @Component({
   selector: 'michackathon-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.scss']
+  /* directives: [DataTableDirectives] */
 })
 export class FlightSearchComponent implements OnInit {
+  data=null;
+ 
+  recommendations: Flights[] = [];
+  flights: Flights[] = [];
+  editing: boolean = true;
+  flightCriteria: FlightCriteria = {
+      departure: 'DXB',
+      arrival: 'BOM',
+      travelDate: new Date(),
+    };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public http: Http, public authService: AuthService) {
+   http.get("app/data/data.json")
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                }, 1000);
+            });
   }
 
+
+  ngOnInit(): any {
+    console.log('hello `flight-search` component');
+  }
+
+
+  public resetCriteria() {
+  console.log('Reseting');
+    this.flightCriteria = {
+      departure: '',
+      arrival: '',
+      travelDate: null,
+    };
+    this.flights=[];
+    this.recommendations=[];
+    
+  }
+  public reset() {
+    this.flights=[];
+    this.flightCriteria = {
+      departure: '',
+      arrival: '',
+      travelDate: null,
+    };
+  }
+
+
+
+  public search(criteria : FlightCriteria) {
+    console.log('in Search');
+    this.searchFlights(criteria);
+    this.fetchRecommandations()
+  }
+
+  private searchFlights(criteria : FlightCriteria) {
+    //this.http.post('/api/flights/',criteria, {headers: this.authService.getAuthorizationHeaders()})
+    console.log('searchFlights')
+    this.http.get("app/data/searchData.json")
+      .subscribe(
+        data => {
+          this.flights = data.json();
+        },
+        err => console.log('Something went wrong')
+      );
+  }
+  
+   private fetchRecommandations() {
+    
+   // this.http.get('/api/recommendations/', {headers: this.authService.getAuthorizationHeaders()})
+    this.http.get("app/data/recommendations.json")
+      .subscribe(
+        data => {
+          this.recommendations = data.json();
+        },
+        err => console.log('Something went wrong')
+      );
+  }
+
+}
+
+export interface Flights {
+  departure: string;
+  arrival: string;
+  departureDate: Date;
+  arrivalDate: Date;
+  price: number;
+}
+
+export interface FlightCriteria {
+  departure: string;
+  arrival: string;
+  travelDate: Date;
 }
